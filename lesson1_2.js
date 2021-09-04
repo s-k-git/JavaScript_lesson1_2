064dbb85b29db7adc9e741f2b861c27a9690cff5
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-09-04 17:57:21 +0900

    【JavaScript課題①-2のレビュー依頼】

--- lesson1_2.js
+++ lesson1_2.js
@@ -2,12 +2,8 @@ var capital = prompt("日本の首都は？");
 while (true) {
     if (capital === "東京") {
         alert("正解です！");
-        break;
-    } else if (capital === null) {
-        alert("キャンセルボタンが押されました");
-        break;
     } else {
         alert("不正解です！");
-        capital = prompt("日本の首都は？");
     }
+    capital = prompt("日本の首都は？");
 }
