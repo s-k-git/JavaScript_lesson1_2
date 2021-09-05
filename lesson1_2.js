c6e96c541897a4e01e63d123907ea69207543e88
s-k-git <89926320+s-k-git@users.noreply.github.com>
2021-09-05 14:17:02 +0900

    varからconstに変更いたしました

--- lesson1_2.js
+++ lesson1_2.js
@@ -1,9 +1,12 @@
-var capital = prompt("日本の首都は？");
 while (true) {
+    const capital = prompt("日本の首都は？");
     if (capital === "東京") {
         alert("正解です！");
+        break;
+    } else if (capital === null) {
+        alert("キャンセルボタンが押されました");
+        break;
     } else {
         alert("不正解です！");
     }
-    capital = prompt("日本の首都は？");
 }
